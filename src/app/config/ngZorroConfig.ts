b70d1fbf3f64a4c489fc90dfa99c508d93ac5249
SUS0a <tom969805235@163. com>
2019-11-02 21:00:59 +0800

    暂时全局配置不需要抽取，之后app.module臃肿再改

---
+++ src/app/config/ngZorroConfig.ts
@@ -0,0 +1,10 @@
+// import {NzConfig} from 'ng-zorro-antd';
+//
+//
+// export class NgZorroConfig {
+//   const ngZorroConfig: NzConfig = {
+//     // 注意没有 nz 前缀
+//     message: { nzTop: 120 },
+//     notification: { nzTop: 240 }
+//   }
+// }
